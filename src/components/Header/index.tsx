de9d204c6d22fb15f9c06865cb15f659baaba60c
Maciel Azevedo <maciel.az.neto@gmail.com>
2023-03-22 22:01:02 -0300

    Header component added

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,16 @@
+import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
+import logoImage from "../../assets/logo.svg"
+
+export function Header() {
+    return (
+        <HeaderContainer>
+            <HeaderContent>
+                <img src={logoImage} alt="" />
+
+                <NewTransactionButton>
+                    Nova Transação
+                </NewTransactionButton>
+            </HeaderContent>
+        </HeaderContainer>
+    )
+}
