dee75e0c7c217f158db041b57892ce36da492eed
Maciel Azevedo <maciel.az.neto@gmail.com>
2023-03-23 22:15:58 -0300

    Transactions context create

--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -1,10 +1,28 @@
 import { MagnifyingGlass } from "phosphor-react";
+import { useForm } from "react-hook-form";
 import { SearchFormContainer } from "./styles";
+import * as z from 'zod'
+
+const searchFormSchema = z.object({
+    query: z.string(),
+})
+
+type SearchFormInputs = z.infer<typeof searchFormSchema>
 
 export function SearchForm() {
+    const { register, handleSubmit } = useForm<SearchFormInputs>()
+
+    function handleSearchTransactions() {
+
+    }
+
     return (
-        <SearchFormContainer>
-            <input type="text" placeholder="Busque por transações" />
+        <SearchFormContainer onSubmit={handleSubmit(handleSearchTransactions)}>
+            <input
+                type="text"
+                placeholder="Busque por transações"
+                {...register("query")}
+            />
 
             <button type="submit">
                 <MagnifyingGlass size={20} />
--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,9 +1,14 @@
+import { useContext } from "react";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { TransactionsContext } from "../../contexts/TransactionsContext";
+import { dateFormatter, priceFormatter } from "../../utils/formatter";
 import { SearchForm } from "./components/SearchForm";
 import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 
+
 export function Transactions() {
+    const { transactions } = useContext(TransactionsContext)
     return (
         <div>
             <Header />
@@ -14,27 +19,21 @@ export function Transactions() {
 
                 <TransactionsTable>
                     <tbody>
-                        <tr>
-                            <td width="50%">Desenvolvimento de site</td>
-                            <td>
-                                <PriceHighlight variant="income">
-                                    R$ 12.000,00
-                                </PriceHighlight>
-                            </td>
-                            <td>Venda</td>
-                            <td>13/04/2022</td>
-                        </tr>
-                        <tr>
-                            <td width="50%">Lasanha</td>
-                            <td>
-                                <PriceHighlight variant="outcome">
-                                    - R$ 59,00
-                                </PriceHighlight>
-
-                            </td>
-                            <td>Alimentação</td>
-                            <td>13/04/2022</td>
-                        </tr>
+                        {transactions.map(({ id, description, type, price, category, createdAt }) => {
+                            return (
+                                <tr key={id}>
+                                    <td width="50%">{description}</td>
+                                    <td>
+                                        <PriceHighlight variant={type}>
+                                            {type === 'outcome' && '- '}
+                                            {priceFormatter.format(price)}
+                                        </PriceHighlight>
+                                    </td>
+                                    <td>{category}</td>
+                                    <td>{dateFormatter.format(new Date(createdAt))}</td>
+                                </tr>
+                            )
+                        })}
                     </tbody>
                 </TransactionsTable>
             </TransactionsContainer>
