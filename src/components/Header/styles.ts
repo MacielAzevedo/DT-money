336726f2a78ab88a5cb79bc9dea5c9e6c256e286
Maciel Azevedo <maciel.az.neto@gmail.com>
2023-03-31 15:44:39 -0300

    optimizing application performance with useCallback, memo and useMemo

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,9 +1,9 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
 export const HeaderContainer = styled.header`
-  background: ${({ theme }) => theme["gray-900"]};
+  background: ${({ theme }) => theme['gray-900']};
   padding: 2.5rem 0 7.5rem;
-`;
+`
 
 export const HeaderContent = styled.div`
   width: 100%;
@@ -19,12 +19,12 @@ export const HeaderContent = styled.div`
     flex-direction: column;
     gap: 1rem;
   }
-`;
+`
 
 export const NewTransactionButton = styled.button`
   height: 50px;
   border: 0;
-  background: ${({ theme }) => theme["green-500"]};
+  background: ${({ theme }) => theme['green-500']};
   color: ${({ theme }) => theme.white};
   font-weight: bold;
   padding: 0 1.125rem;
@@ -32,7 +32,7 @@ export const NewTransactionButton = styled.button`
   cursor: pointer;
 
   &:hover {
-    background: ${({ theme }) => theme["green-700"]};
+    background: ${({ theme }) => theme['green-700']};
     transition: background-color 0.2s;
   }
-`;
+`
--- src/components/Summary/styles.ts
+++ src/components/Summary/styles.ts
@@ -1,4 +1,4 @@
-import styled, { css } from "styled-components";
+import styled, { css } from 'styled-components'
 
 export const SummaryContainer = styled.section`
   width: 100%;
@@ -18,14 +18,14 @@ export const SummaryContainer = styled.section`
   @media (min-width: 451px) and (max-width: 768px) {
     grid-template-columns: 1fr;
   }
-`;
+`
 
 interface SummaryCardProps {
-  variant?: "green";
+  variant?: 'green'
 }
 
 export const SummaryCard = styled.div<SummaryCardProps>`
-  background-color: ${({ theme }) => theme["gray-600"]};
+  background-color: ${({ theme }) => theme['gray-600']};
   border-radius: 6px;
   padding: 2rem;
 
@@ -33,7 +33,7 @@ export const SummaryCard = styled.div<SummaryCardProps>`
     display: flex;
     align-items: center;
     justify-content: space-between;
-    color: ${({ theme }) => theme["gray-300"]};
+    color: ${({ theme }) => theme['gray-300']};
   }
 
   strong {
@@ -43,8 +43,8 @@ export const SummaryCard = styled.div<SummaryCardProps>`
   }
 
   ${({ variant }) =>
-    variant === "green" &&
+    variant === 'green' &&
     css`
-      background-color: ${({ theme }) => theme["green-700"]};
+      background-color: ${({ theme }) => theme['green-700']};
     `}
-`;
+`
